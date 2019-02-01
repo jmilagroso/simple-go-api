844cd51f96ff8655ba17c8d097441a0af8e6e790
Jay <j.milagroso@gmail.com>
2019-02-01 14:46:21 +0800

    Add response GoVersion.

--- jmilagroso/goberks/routes/index.go
+++ jmilagroso/goberks/routes/index.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"runtime"
 	"time"
 
 	h "quadx.xyz/jmilagroso/goberks/helpers"
@@ -15,9 +16,10 @@ import (
 
 type Index struct {
 	ServerTime string
+	GoVersion  string
 }
 
 // GetIndex - Get index route
 func GetIndex(w http.ResponseWriter, r *http.Request) {
-	h.Error(json.NewEncoder(w).Encode(Index{ServerTime: time.Now().String()}))
+	h.Error(json.NewEncoder(w).Encode(Index{ServerTime: time.Now().String(), GoVersion: runtime.Version()}))
 }
