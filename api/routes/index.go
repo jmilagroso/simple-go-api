fec1919deabe3192f8755cdcb6e9ef943ce70f88
Jay <j.milagroso@gmail.com>
2019-02-09 22:29:47 +0800

    Removed tile38 dependencies.

--- api/routes/index.go
+++ api/routes/index.go
@@ -9,27 +9,21 @@ import (
 
 	"github.com/gorilla/mux"
 	h "github.com/jmilagroso/api/helpers"
-	"github.com/jmilagroso/api/models"
+	m "github.com/jmilagroso/api/models"
 )
 
-// / Response struct
-type Index struct {
-	ServerTime string `json:"server_time"`
-	GoVersion  string `json:"go_version"`
-}
-
 // IndexDBClient db client(s) local type
-type IndexDBClient models.DBClient
+type IndexDBClient m.DBClient
 
 // GetIndex - Get index route
 func GetIndex(w http.ResponseWriter, r *http.Request) {
-	h.Error(json.NewEncoder(w).Encode(Index{ServerTime: time.Now().String(), GoVersion: runtime.Version()}))
+	h.Error(json.NewEncoder(w).Encode(m.Index{ServerTime: time.Now().String(), GoVersion: runtime.Version()}))
 }
 
 // GetUsers - Get users route
 func (dbClient *IndexDBClient) GetUsers(w http.ResponseWriter, r *http.Request) {
 	// Collection container
-	var rows []models.User
+	var rows []m.User
 
 	// @TODO Inject Caching using Heroku Redis (Needs account upgrade)
 	// @TODO Cache page_per_page = 1_10
@@ -57,7 +51,7 @@ func (dbClient *IndexDBClient) GetUsersPaginated(w http.ResponseWriter, r *http.
 	// @TODO Cache timeout 30 secs
 
 	// Output
-	var rows []models.User
+	var rows []m.User
 
 	dbClient.Query(&rows, `SELECT id, username, email FROM users ORDER BY id DESC LIMIT ? OFFSET ?`, perPage, offset)
 
